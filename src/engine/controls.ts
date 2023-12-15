76ffdc8796e6f0c81837d1e69f6a7ee9ebfced12
Aleksei Laud <mail@a-laud.ru>
2023-12-15 19:06:36 +0400

    Add controls

---
+++ src/engine/controls.ts
@@ -0,0 +1,7 @@
+export const initControl = (selector: string, callback: (e: Event) => void) => {
+    const element = document.querySelector(selector);
+    if (!element) {
+        return;
+    }
+    element.addEventListener('click', callback);
+}
--- src/engine/powers/index.ts
+++ src/engine/powers/index.ts
@@ -3,6 +3,14 @@ import { gravityProcessor } from "./gravity";
 import { mouseProcessor } from "./mouse";
 import { TPowerProcessor } from "./powers";
 import { randomnessProcessor } from "./randomness";
+import { initControl } from "../controls";
+
+const availablePowers: Record<string, TPowerProcessor> = {
+    gravity: gravityProcessor,
+    density: densityProcessor,
+    randomness: randomnessProcessor,
+    mouse: mouseProcessor,
+};
 
 export const powers: Array<TPowerProcessor> = [
     gravityProcessor,
@@ -10,3 +18,21 @@ export const powers: Array<TPowerProcessor> = [
     randomnessProcessor,
     mouseProcessor,
 ];
+
+initControl('input#gravity', (e) => {
+    const input = e.target as HTMLInputElement;
+    if (input.checked) {
+        powers.push(availablePowers.gravity);
+    } else {
+        powers.splice(powers.indexOf(availablePowers.gravity), 1);
+    }
+})
+
+initControl('input#density', (e) => {
+    const input = e.target as HTMLInputElement;
+    if (input.checked) {
+        powers.push(availablePowers.density);
+    } else {
+        powers.splice(powers.indexOf(availablePowers.density), 1);
+    }
+})
