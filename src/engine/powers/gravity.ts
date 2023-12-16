82d8f0e68d669efc56daef063f6bf4d8cfbbf90d
Aleksei Laud <mail@a-laud.ru>
2023-12-16 14:24:15 +0400

    try accelerometer

--- src/engine/powers/gravity.ts
+++ src/engine/powers/gravity.ts
@@ -1,6 +1,34 @@
+import { TVector } from "../data.t";
 import { TPowerProcessor } from "./powers";
 
 const GRAVITY_ACCELERATION = 9.8
+
+let currentAcceleration: TVector = {
+    x: 0,
+    y: GRAVITY_ACCELERATION
+}
+
+function handleMotion(event) {
+    currentAcceleration.x = event.accelerationIncludingGravity.x || 0;
+    currentAcceleration.y = event.accelerationIncludingGravity.y || GRAVITY_ACCELERATION;
+}
+
+const getAccelerometerDirection = (): TVector => {
+    return currentAcceleration
+}
+
+
+// Request permission for iOS 13+ devices
+if (
+    DeviceMotionEvent &&
+    // @ts-ignore
+    typeof DeviceMotionEvent.requestPermission === "function"
+) {
+    // @ts-ignore
+    DeviceMotionEvent.requestPermission();
+}
+
+window.addEventListener("devicemotion", handleMotion);
 
 export const gravityProcessor: TPowerProcessor = (point) => {
     point.acceleration.y += GRAVITY_ACCELERATION
