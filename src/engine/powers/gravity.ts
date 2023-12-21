12091f52c72e8faec5c0beb5245b589237f23bb2
Aleksei Laud <mail@a-laud.ru>
2023-12-21 17:21:44 +0400

    Gravity to the center

--- src/engine/powers/gravity.ts
+++ src/engine/powers/gravity.ts
@@ -1,8 +1,15 @@
+import { BORDERS } from "../constants";
 import { initControl } from "../controls";
 import { TVector } from "../data.t";
+import { getVectorLength, multiplyVector } from "../utils/vector";
 import { TPowerProcessor } from "./powers";
 
 let GRAVITY_ACCELERATION = 9.8
+let isCentered = false
+
+initControl('input#gravityCentered', (e) => {
+    isCentered = (e.target as HTMLInputElement)?.checked ?? false
+})
 
 let currentAcceleration: TVector = {
     x: 0,
@@ -32,6 +39,7 @@ document.querySelector('input#gravity')?.addEventListener('click', async () => {
         try {
             // @ts-ignore
             await DeviceMotionEvent.requestPermission();
+            document.querySelector('.gravityPowerLabel')?.remove()
         } catch (e) {
             console.log(e.message || e.toString())
         }
@@ -40,7 +48,29 @@ document.querySelector('input#gravity')?.addEventListener('click', async () => {
 
 window.addEventListener("devicemotion", handleMotion, true);
 
+const CENTER_OF_THE_SCREEN: TVector = {
+    x: BORDERS.maxX / 2,
+    y: BORDERS.maxY / 2
+}
+
 export const gravityProcessor: TPowerProcessor = (point) => {
+    if (isCentered) {
+        const directionToTheCenter: TVector = {
+            x: CENTER_OF_THE_SCREEN.x - point.position.x,
+            y: CENTER_OF_THE_SCREEN.y - point.position.y,
+        }
+
+        const directionToTheCenterLength = getVectorLength(directionToTheCenter)
+
+        const directionToTheCenterNormalized = multiplyVector(directionToTheCenter, 1 / directionToTheCenterLength)
+
+        const gravityForce = multiplyVector(directionToTheCenterNormalized, GRAVITY_ACCELERATION)
+
+        point.acceleration.x += gravityForce.x
+        point.acceleration.y += gravityForce.y
+        return
+    }
+
     const { x, y } = getAccelerometerDirection()
     point.acceleration.y += y
     point.acceleration.x += x
