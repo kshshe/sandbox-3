1d1d446b5cf37dd2bcb01892bc43915bf42fe3b7
Aleksei Laud <mail@a-laud.ru>
2024-01-10 12:47:28 +0400

    Show velocity

--- src/engine/render.ts
+++ src/engine/render.ts
@@ -7,6 +7,7 @@ import { getVectorLength } from "./utils/vector";
 
 let customSizes = false;
 let showArrows = false;
+let showSpeedArrows = false;
 
 initControl('input#custom-sizes', (e) => {
     const input = e.target as HTMLInputElement;
@@ -16,6 +17,11 @@ initControl('input#custom-sizes', (e) => {
 initControl('input#show-arrows', (e) => {
     const input = e.target as HTMLInputElement;
     showArrows = input.checked;
+})
+
+initControl('input#show-arrows-2', (e) => {
+    const input = e.target as HTMLInputElement;
+    showSpeedArrows = input.checked;
 })
 
 export const initRender = () => {
@@ -282,6 +288,51 @@ export const initRender = () => {
                 const normalizedYCord = 1 - 2 * point.position.y / BORDERS.maxY;
                 const arrowEndX = normalizedXCord + (point.acceleration.x / BORDERS.maxX) * arrowSize;
                 const arrowEndY = normalizedYCord - (point.acceleration.y / BORDERS.maxY) * arrowSize;
+                return [
+                    normalizedXCord,
+                    normalizedYCord,
+                    arrowEndX,
+                    arrowEndY
+                ];
+            }).flat();
+        
+            const arrowVertexBuffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, arrowVertexBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arrowPoints), gl.STATIC_DRAW);
+        
+            const arrowPositionAttributeLocation = gl.getAttribLocation(program, 'a_position');
+            gl.enableVertexAttribArray(arrowPositionAttributeLocation);
+            gl.vertexAttribPointer(arrowPositionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+        
+            const arrowColorBuffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, arrowColorBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arrowColor), gl.STATIC_DRAW);
+        
+            const arrowColorAttributeLocation = gl.getAttribLocation(program, 'a_color');
+            gl.enableVertexAttribArray(arrowColorAttributeLocation);
+            gl.vertexAttribPointer(arrowColorAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+        
+            const arrowSizeBuffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, arrowSizeBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(Array(points.length).fill(arrowSize)), gl.STATIC_DRAW);
+        
+            const arrowSizeAttributeLocation = gl.getAttribLocation(program, 'a_size');
+            gl.enableVertexAttribArray(arrowSizeAttributeLocation);
+            gl.vertexAttribPointer(arrowSizeAttributeLocation, 1, gl.FLOAT, false, 0, 0);
+        
+            gl.drawArrays(gl.LINES, 0, arrowPoints.length / 2);
+        }
+
+        if (showSpeedArrows) {
+            // Render velocity arrows
+            const arrowSize = 3;
+            const arrowColor = [0, 0, 1]; // Blue color for arrows
+        
+            const arrowPoints = points.map((point) => {
+                const normalizedXCord = 2 * point.position.x / BORDERS.maxX - 1;
+                const normalizedYCord = 1 - 2 * point.position.y / BORDERS.maxY;
+                const arrowEndX = normalizedXCord + (point.velocity.x / BORDERS.maxX) * arrowSize;
+                const arrowEndY = normalizedYCord - (point.velocity.y / BORDERS.maxY) * arrowSize;
                 return [
                     normalizedXCord,
                     normalizedYCord,
